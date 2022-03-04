efe55ef9c1432cc4935ea61ef8faa0318773163a
CyberSecurity0110 <galaxyman0110@gmail.com>
2022-03-04 22:24:39 +0330

    url_shortner-app

--- urlshortnerApp/urls.py
+++ urlshortnerApp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import url_shortner
 
 urlpatterns = [
-
 	#path('',views.url_shortner,name="url-shortner"),
-	path('',url_shortner.as_view(),name="url-shortner"),
-
+	path('', url_shortner.as_view(), name="url-shortner"),
 ]
