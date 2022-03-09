92d29b61cb017f80fbabda1f1678ba61996ef806
CyberSecurity0110 <galaxyman0110@gmail.com>
2022-03-09 23:06:06 +0330

    url_shortner-app

--- urlshortnerApp/views.py
+++ urlshortnerApp/views.py
@@ -1,5 +1,6 @@
 
 # import suitable packages
+from multiprocessing import context
 from django.shortcuts import render
 import pyshorteners
 from django.views import View
@@ -22,14 +23,26 @@ def url_shortner(request):
 
 # Class Based Views to perform url shortner
 
+# class url_shortner(View):
+
+# 	def post(self, request):
+# 		long_url = 'url' in request.POST and request.POST['url']
+# 		pys = pyshorteners.Shortener()
+# 		short_url = pys.osdb.short(long_url)
+# 		return render(request, 'urlShortner.html', context={'short_url': short_url, 'long_url': long_url})
+
+# 	def get(self, request):
+# 		return render(request, 'urlShortner.html')
+
 class url_shortner(View):
-
-	def post(self, request):
-		long_url = 'url' in request.POST and request.POST['url']
-		pys = pyshorteners.Shortener()
-		short_url = pys.osdb.short(long_url)
-		return render(request, 'urlShortner.html', context={'short_url': short_url,'long_url': long_url})
-
-	def get(self, request):
-		return render(request, 'urlShortner.html')
-		
+    
+    def get(self, request):
+        return render(request, 'urlShortner.html')
+    
+    def post(self, request):
+        long_url = 'url' in request.POST and request.POST['url']
+        pys = pyshorteners.Shortener()
+        short_url = pys.osdb.short(long_url)
+        context = {'short_url': short_url,
+                   'long_url': long_url}
+        return render(request, 'urlShortner.html', context)
